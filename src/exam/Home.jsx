0bc76fb12e2b32a9b353fab6aa28489639993f44
karthik <2500031901@kluniversity.in>
2026-03-13 12:38:34 +0530

    added image in Home.jsx

--- src/exam/Home.jsx
+++ src/exam/Home.jsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div>
-      <h3>Home Page</h3>
+    <div style={{textAlign:"center"}}>
+
+      <h3>Welcome to Event Management System</h3>
+
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/024/316/125/large_2x/event-management-wedding-planner-manager-planning-event-conference-or-party-professional-organizer-schedule-modern-flat-cartoon-style-illustration-on-white-background-vector.jpg"
+        alt="Event Management"
+        width="600"
+      />
+
     </div>
   );
 }
